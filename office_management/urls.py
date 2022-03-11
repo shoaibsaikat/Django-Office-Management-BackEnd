a7c4f067530b81de6a2dbe33a877b704427a8c2f
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2022-03-11 12:40:58 +0600

    added jwt urls

--- office_management/urls.py
+++ office_management/urls.py
@@ -17,9 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from . import views
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('user/', include('accounts.urls')),
     path('inventory/', include('inventory.urls')),
